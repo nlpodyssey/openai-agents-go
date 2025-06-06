98335d3df4519e60d9e11b9d828ac3873354b964
Marco Nicola <marconicola@disroot.org>
2025-06-06 23:08:20 +0200

    Prefer param.Opt to Optional for FunctionTool.StrictJSONSchema
    The ultimate goal is to replace Optional with param.Opt in the whole project, to keep it more seamlessly integrated with openai-go

--- examples/basic/stream_items/stream_items.go
+++ examples/basic/stream_items/stream_items.go
@@ -21,7 +21,6 @@ import (
 
 	"github.com/nlpodyssey/openai-agents-go/agents"
 	"github.com/nlpodyssey/openai-agents-go/runcontext"
-	"github.com/nlpodyssey/openai-agents-go/types/optional"
 	"github.com/openai/openai-go/packages/param"
 )
 
@@ -42,7 +41,7 @@ var HowManyJokesTool = agents.FunctionTool{
 	OnInvokeTool: func(_ context.Context, _ *runcontext.RunContextWrapper, arguments string) (any, error) {
 		return HowManyJokes(), nil
 	},
-	StrictJSONSchema: optional.Value(true),
+	StrictJSONSchema: param.NewOpt(true),
 }
 
 func main() {
